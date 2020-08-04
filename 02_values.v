60347d21547f66668dcdc9b752831bad7547793f
Aibhstin <pickfora@uni.coventry.ac.uk>
2020-08-04 02:06:54 +0100

    first commit

---
+++ 02_values.v
@@ -0,0 +1,10 @@
+fn main() {
+	println("v" + "lang")
+
+	println("1 + 1 = " + (1 + 1).str())
+	println("7.0 / 3.0 = " + (7.0 / 3.0).str())
+
+	println(true && false)
+	println(true || false)
+	println(!true)
+}
