24a28d1d3721b5521c56e812cae38d021505ac01
Aibhstin <pickfora@uni.coventry.ac.uk>
2020-08-04 02:19:00 +0100

    add 03 and 04

---
+++ 03_variables.v
@@ -0,0 +1,16 @@
+fn main() {
+	a := "initial"
+	println(a)
+
+	b, c := 1, 2
+	println(b.str() + " " + c.str())
+
+	d := true
+	println(d)
+
+	e := i64(0)
+	println(e)
+
+	f := "apple"
+	println(f)
+}
---
+++ 04_constants.v
@@ -0,0 +1,17 @@
+import math
+
+const (
+	s = "constant"
+	n = 500000000
+	d = 3e20 / n
+)
+
+fn main() {
+	println(s)
+
+	println(d)
+
+	println(i64(d))
+
+	println(math.sin(n))
+}
